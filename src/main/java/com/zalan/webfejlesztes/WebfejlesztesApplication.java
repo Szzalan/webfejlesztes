7caa352a416025d584d181aa5dee17a97d9c4f56
Szzalan <zalanka12345678@gmail.com>
2024-12-21 22:20:38 +0100

    initial commit

---
+++ src/main/java/com/zalan/webfejlesztes/WebfejlesztesApplication.java
@@ -0,0 +1,15 @@
+package com.zalan.webfejlesztes;
+
+import lombok.extern.java.Log;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Log
+public class WebfejlesztesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfejlesztesApplication.class, args);
+	}
+
+}
